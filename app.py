24b0cc35d1a2fbde3daff7dfacee2c9e1037f1db
Lud <ludmilamiguenz@hotmail.com>
2026-06-05 15:45:38 -0300

    Dockerizado servidor Flask

--- app.py
+++ app.py
@@ -111,5 +111,5 @@ def delete_text(news_id):
     }), 200
 
 
-if __name__ == "__main__":  # Se ejecuta solo si este archivo se corre directamente
-    app.run(debug=True, port=5000)  # Inicia el servidor en modo debug en el puerto 5000
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
